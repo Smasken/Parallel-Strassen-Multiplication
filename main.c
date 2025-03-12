e29b8d4f7980c98601b9269989362f1dbff38d34
Dan Friedman <dan.fri66@gmail.com>
2025-03-12 15:09:52 +0100

    makefile and functioning naive multiplication

--- main.c
+++ main.c
@@ -14,17 +14,17 @@ typedef float data_type;
 
 // Allocates memory for a size*size matrix
 data_type **allocate_matrix(int size) {
-   data_type **matrix = (data_type**)malloc(sizeof(data_type)*size);
+   data_type **matrix = (data_type**)malloc(sizeof(data_type*) * size);
    for(int i = 0; i < size; i++) {
-      matrix[i] = (data_type*)malloc(sizeof(data_type)*size);
+      matrix[i] = (data_type*)malloc(sizeof(data_type) * size);
    }
-  return matrix;
+   return matrix;
 }
 
 void fill_matrix(int size, data_type **matrix) {
     for(int i = 0; i < size; i++) {
         for(int j = 0; j < size; j++) {
-            matrix[i][j] = rand() % 1000;
+            matrix[i][j] = (data_type)(rand() % 1000);
         }
     }
 }
@@ -50,7 +50,7 @@ void standard_matrix_multiplication(int size, data_type **A, data_type **B, data
 
 void add_matrix(int size, data_type **A, data_type **B, data_type **C){
    for(int i = 0; i < size; i++) {
-      for (int j = 0; j < size; i++) {
+      for (int j = 0; j < size; j++) {
          C[i][j] = A[i][j] + B[i][j];
       }
    }
@@ -58,7 +58,7 @@ void add_matrix(int size, data_type **A, data_type **B, data_type **C){
 
 void subtract_matrix(int size, data_type **A, data_type **B, data_type **C){
    for(int i = 0; i < size; i++) {
-      for (int j = 0; j < size; i++) {
+      for (int j = 0; j < size; j++) {
          C[i][j] = A[i][j] - B[i][j];
       }
    }
@@ -69,6 +69,10 @@ void strassen_multiplication(int size, data_type **A, data_type **B, data_type *
 }
 
 int main(int argc, char *argv[]) {
+   if (argc != 2) {
+      printf("Input should be: ./a.out [matrix size]\n");
+      return -1;
+   }
 
    int size = atoi(argv[1]); //Input argument is matrix size
 
@@ -81,10 +85,19 @@ int main(int argc, char *argv[]) {
 
    double start_time = get_wall_seconds();
 
-   standard_matrix_multiplication(size, A, B, C);
+   // Strassen only used for large matrices
+   if(size < 256) {
+      standard_matrix_multiplication(size, A, B, C);
+   } else {
+      strassen_multiplication(size, A, B, C);
+   }
 
    double end_time = get_wall_seconds();
    printf("Time taken: %f seconds\n", end_time - start_time);
+
+   deallocate_matrix(size, A);
+   deallocate_matrix(size, B);
+   deallocate_matrix(size, C);
 
    return 0;
 }
