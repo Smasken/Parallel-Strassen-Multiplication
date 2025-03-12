c2458ec98579f86d9ecc7d3b694e4963ebe37d67
Dan Friedman <dafr9906@fredholm.it.uu.se>
2025-03-12 12:55:03 +0100

    allocation function

--- main.c
+++ main.c
@@ -1,46 +1,40 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+// This allows me to change what to fill the matrix with (int, float, etc.)
+typedef float data_type;
+
+// This function allocates memory for a size*size matrix and fills it with random floats.
+data_type **allocate_matrix(int size) {
+   data_type **matrix = (data_type**)malloc(sizeof(data_type)*size);
+   for(int i = 0; i < size; i++) {
+      matrix[i] = (data_type*)malloc(sizeof(data_type)*size);
+   }
+
+   for(int i = 0; i < size; i++) { // Fills matrix
+      for(int j = 0; j < size; j++) {
+          matrix[i][j] = rand() % 100;
+      }
+  }
+  return matrix;
+}
+
+void deallocate_matrix() {
+
+// Standard algorithm - multiplies A and B, C is output
+}
+void standard_matrix_multiplication(int size, data_type **A, data_type **B, data_type **C) {
+   for(int i = 0; i < size; i++) {
+      for (int j = 0; j < size; j++) {
+         C[i][j]=0;
+         for(int k = 0; k < size; k++) {
+            C[i][j] += A[i][k] * B[k][j];
+         }
+      }
+   }
+}
 
 int main(){
-   int z[2][2];
-   int i, j;
-   int m1, m2, m3, m4 , m5, m6, m7;
-   int x[2][2] = {
-       {12, 34}, 
-       {22, 10}
-       };
-   int y[2][2] = {
-       {3, 4}, 
-       {2, 1}
-   };
-   printf("The first matrix is: ");
-   for(i = 0; i < 2; i++) {
-      printf("\n");
-      for(j = 0; j < 2; j++)
-         printf("%d\t", x[i][j]);
-   }
-   printf("\nThe second matrix is: ");
-   for(i = 0; i < 2; i++) {
-      printf("\n");
-      for(j = 0; j < 2; j++)
-         printf("%d\t", y[i][j]);
-   }
-   m1= (x[0][0] + x[1][1]) * (y[0][0] + y[1][1]);
-   m2= (x[1][0] + x[1][1]) * y[0][0];
-   m3= x[0][0] * (y[0][1] - y[1][1]);
-   m4= x[1][1] * (y[1][0] - y[0][0]);
-   m5= (x[0][0] + x[0][1]) * y[1][1];
-   m6= (x[1][0] - x[0][0]) * (y[0][0]+y[0][1]);
-   m7= (x[0][1] - x[1][1]) * (y[1][0]+y[1][1]);
-   z[0][0] = m1 + m4- m5 + m7;
-   z[0][1] = m3 + m5;
-   z[1][0] = m2 + m4;
-   z[1][1] = m1 - m2 + m3 + m6;
-   printf("\nProduct achieved using Strassen's algorithm: ");
-   for(i = 0; i < 2 ; i++) {
-      printf("\n");
-      for(j = 0; j < 2; j++)
-         printf("%d\t", z[i][j]);
-   }
-   printf("\n");
-   return 0;
+
 }
